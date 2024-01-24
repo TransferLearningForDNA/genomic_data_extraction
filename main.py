e62e91ffbb81bfcd1e8a87c4b7fd6c0d36a0e72d
Rada Djoneva <ryd23@ic.ac.uk>
2024-01-24 21:00:52 +0000

    Started constructing the main structure of main.py and added Helenas code.

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+""" Genomic data extraction pipeline."""
 
+# Import the Ensembl API module
+from dna import ensembl_api
+
+# DNA: Extracting genomic data.
+
+# TODO Helena's par
+
+# Specify the folder containing gene lists
+folder = "dna/gene_lists/"
+
+# Specify the list of file paths for gene lists
+# Original: file_paths = ["homo_sapiens_genes.txt", "saccharomyces_cerevisiae_genes.txt"]
+file_paths = ["homo_sapiens_genes_small.txt"]
+
+# Prepend the folder path to each file path
+file_paths = [folder + path for path in file_paths]
+
+# TODO save CSV files directly to One Drive?
+# Call the function to get data from Ensembl API and save it as CSV files
+ensembl_api.get_data_as_csv(file_paths, "dna/csv_files")
+
+
+# RNA: Extracting mRNA expression levels.
