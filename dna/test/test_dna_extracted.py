0d877e3336b6b567106890c82232c942378512c2
Alejandro Ayuso Garcia <alejandro.ayuso-garcia23@imperial.ac.uk>
2024-02-04 14:47:44 +0100

    SCRUM-27: Replaced test for homo sapiens with a new test that checks extracted components automatically for all genes of the species in gene_lists

--- dna/test/test_dna_extracted.py
+++ dna/test/test_dna_extracted.py
@@ -23,33 +23,40 @@ def test_get_species_name():
     species_name_true = "homo_sapiens"
     assert species_name == species_name_true
 
-def test_extract_homo_sapiens_dna_components():
-    # Extract ground truth DNA sequence components from csv file
-    filepath_true = "ground_truth_components/true_homo_sapiens_dna.csv"
-    filepath_true = os.path.join(os.path.dirname(__file__), filepath_true)
-    with open(filepath_true) as file:
-        reader = csv.DictReader(file)
-        dna_extracted_true = [gene for gene in reader]
-    
+def test_check_extracted_components():
     # Extract DNA sequence components from csv file using ensembl_api
-    gene_ids_filepaths = ["gene_lists/homo_sapiens_genes.txt"]
-    gene_ids_filepaths = [os.path.join(os.path.dirname(__file__), filepath) for filepath in gene_ids_filepaths]
+    gene_ids_folderpath = "gene_lists"
+    gene_ids_folderpath = os.path.join(os.path.dirname(__file__), gene_ids_folderpath)
+    gene_ids_filelist = os.listdir(gene_ids_folderpath)
     output_filepath = "csv_files"
-    ensembl_api.get_data_as_csv(gene_ids_filepaths, output_filepath)
-    filepath_test = output_filepath + "/" + "ensembl_data_homo_sapiens.csv"
-    with open(filepath_test) as file:
-        reader = csv.DictReader(file)
-        dna_extracted_test = [gene for gene in reader]
-
-    # Check that extracted components and ground truth components match
-    for (dna_true, dna_test) in zip(dna_extracted_true, dna_extracted_test):
-        assert dna_true["promoter"]==dna_test["promoter"]
-        assert dna_true["utr5"]==dna_test["utr5"]
-        assert dna_true["cds"]==dna_test["cds"]
-        assert dna_true["utr3"]==dna_test["utr3"]
-        assert dna_true["terminator"]==dna_test["terminator"]
-
-ensembl_api.get_data_as_csv(["gene_lists/saccharomyces_cerevisiae_genes.txt"], "csv_files")
-ensembl_api.get_data_as_csv(["gene_lists/galdieria_sulphuraria_genes.txt"], "csv_files")
-ensembl_api.get_data_as_csv(["gene_lists/chlamydomonas_reinhardtii_genes.txt"], "csv_files")
-ensembl_api.get_data_as_csv(["gene_lists/cyanidioschyzon_merolae_genes.txt"], "csv_files")
+    for species in gene_ids_filelist:
+        gene_ids_filepath = gene_ids_folderpath + "/" + species
+        ensembl_api.get_data_as_csv([gene_ids_filepath], output_filepath)
+    
+    # Loop through each file of extracted DNA and check against ground truth files
+    species_names = ['_'.join(species.split('_')[:2]) for species in gene_ids_filelist]
+    for species in species_names:
+        print("Checking DNA components for: ", species)
+        extracted_dna_filepath = output_filepath + "/ensembl_data_" + species + ".csv"
+        extracted_dna_filepath = os.path.join(os.path.dirname(__file__), extracted_dna_filepath)
+        ground_truth_dna_filepath = "ground_truth_components/true_" + species + "_dna.csv"
+        ground_truth_dna_filepath = os.path.join(os.path.dirname(__file__), ground_truth_dna_filepath)
+        
+        try:
+            with open(extracted_dna_filepath) as file:
+                reader = csv.DictReader(file)
+                dna_extracted_test = [gene for gene in reader]
+            
+            with open(ground_truth_dna_filepath) as file:
+                reader = csv.DictReader(file)
+                dna_extracted_true = [gene for gene in reader]
+        except FileNotFoundError:
+            print(f"The file for {species} could not be found. Check that a ground truth file and an ensembl data file exist for {species}")
+        else:
+            # Check that extracted components and ground truth components match
+            for (dna_true, dna_test) in zip(dna_extracted_true, dna_extracted_test):
+                assert dna_true["promoter"]==dna_test["promoter"]
+                assert dna_true["utr5"]==dna_test["utr5"]
+                assert dna_true["cds"]==dna_test["cds"]
+                assert dna_true["utr3"]==dna_test["utr3"]
+                assert dna_true["terminator"]==dna_test["terminator"]
