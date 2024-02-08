6d99ea425a5dfdd67a87fac781f81b2683c9ab53
Rada Djoneva <ryd23@ic.ac.uk>
2024-02-08 08:50:43 +0000

    Changed the import of ensembl_api.py in test_dna_extracted.py to account for the the change in the test folder structure. The testing.yaml now containds a single job called test_dna instead of test.

--- tests/test_dna/test_dna_extracted.py
+++ tests/test_dna/test_dna_extracted.py
@@ -1,11 +1,14 @@
 """ DNA sequence extraction test_dna-suite."""
 
-import pytest # Import pytest for automated testing
+import pytest  # Import pytest for automated testing
 import sys
 import os
 import csv
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../dna')))
-import ensembl_api
+from dna import ensembl_api
+
+
 # import ensembl_api # Import the Ensembl API module
 
 
@@ -23,15 +26,17 @@ def test_read_gene_ids_from_file():
     gene_ids = ensembl_api.read_gene_ids_from_file(filepath)
     assert gene_ids == gene_ids_true
 
+
 def test_get_species_name():
     """ Tests that species name is read correctly from filename by 
     get_species_name in the ensembl API."""
-    
+
     filepath = os.path.join(os.path.dirname(__file__),
                             "gene_lists/homo_sapiens_genes.txt")
     species_name = ensembl_api.get_species_name(filepath)
     species_name_true = "homo_sapiens"
     assert species_name == species_name_true
+
 
 def test_check_extracted_components():
     """Tests that gene components are correctly extracted by the Ensembl API 
@@ -43,7 +48,7 @@ def test_check_extracted_components():
     is automatically verified against a folder containing the csv files with 
     the ground truth dna components for each of the genes processed.
     """
-    
+
     # Define the folder where gene ids of the 5 mandatory species are stored
     gene_ids_folderpath = os.path.join(os.path.dirname(__file__), "gene_lists")
     # Define the folder where the extracted dna sequences should be stored
@@ -55,35 +60,35 @@ def test_check_extracted_components():
     for species in gene_ids_filelist:
         gene_ids_filepath = os.path.join(gene_ids_folderpath, species)
         ensembl_api.get_data_as_csv([gene_ids_filepath], output_filepath)
-    
+
     # Loop through each csv file containing the extracted sequence components 
     # and check against ground truth csv files
     for species in gene_ids_filelist:
         species_name = '_'.join(species.split('_')[:2])
         print("Checking DNA components for: ", species_name)
-        extracted_dna_filepath = os.path.join(output_filepath, 
+        extracted_dna_filepath = os.path.join(output_filepath,
                                               f"ensembl_data_{species_name}.csv")
-        ground_truth_dna_filepath = os.path.join(os.path.dirname(__file__), 
+        ground_truth_dna_filepath = os.path.join(os.path.dirname(__file__),
                                                  f"ground_truth_components/true_{species_name}_dna.csv")
-        
+
         try:
             with open(extracted_dna_filepath) as file:
                 reader = csv.DictReader(file)
                 dna_extracted_test = [gene for gene in reader]
-            
+
             with open(ground_truth_dna_filepath) as file:
                 reader = csv.DictReader(file)
                 dna_extracted_true = [gene for gene in reader]
 
         except FileNotFoundError:
             print(f"The file for {species} could not be found. "
-                f"Check that a ground truth file and an ensembl data file exist for {species}")
+                  f"Check that a ground truth file and an ensembl data file exist for {species}")
         else:
             for (dna_true, dna_test) in zip(dna_extracted_true, dna_extracted_test):
                 assert (
-                    dna_true["promoter"] == dna_test["promoter"]
-                    and dna_true["utr5"] == dna_test["utr5"]
-                    and dna_true["cds"] == dna_test["cds"]
-                    and dna_true["utr3"] == dna_test["utr3"]
-                    and dna_true["terminator"] == dna_test["terminator"]
+                        dna_true["promoter"] == dna_test["promoter"]
+                        and dna_true["utr5"] == dna_test["utr5"]
+                        and dna_true["cds"] == dna_test["cds"]
+                        and dna_true["utr3"] == dna_test["utr3"]
+                        and dna_true["terminator"] == dna_test["terminator"]
                 )
