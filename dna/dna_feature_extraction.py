40b4fb35112a609def1b4bdc335e63f5b21cdab0
Alejandro Ayuso Garcia <alejandro.ayuso-garcia23@imperial.ac.uk>
2024-02-15 23:10:58 +0000

    Implemented computation of GC content of cds, utr5 and utr3

--- dna/dna_feature_extraction.py
+++ dna/dna_feature_extraction.py
@@ -39,12 +39,14 @@ def extract_dna_features(folder_path):
                     # Add data for new columns (assuming new_columns is a list of values)
                     row.update(compute_cds_codon_frequencies(row, codons))
                     row.update(compute_lengths(row))
+                    row.update(compute_gc_content_sequence_components(row))
                     
                     # Write the modified row to the temporary file
                     writer.writerow(row)
             
             # Replace the original file with the temporary file
             shutil.move(temp_file.name, file_path)
+
 
 def compute_cds_codon_frequencies(row, codons):
     """Compute the frequency of every possible codon in the cds
@@ -56,6 +58,9 @@ def compute_cds_codon_frequencies(row, codons):
 
         codons (list): list of strings represesenting all 64 possible codons
         e.g. AGA
+
+    Returns:
+        dict: dictionary of codon frequencies for all 64 possible codons
     """
     
     cds = row.get("cds")
@@ -81,6 +86,9 @@ def compute_lengths(row):
         row (dict): row extracted from the csv file. This contains the
         following fields: ensembl_gene_id, transcript_id, promoter, utr5, cds,
         utr3, terminator
+
+    Returns:
+        dict: dictionary of lengths of cds, utr5 and utr3
     """
     lengths = {"cds_length" : len(row.get("cds")),
                "utr5_length" : len(row.get("utr5")),
@@ -88,6 +96,50 @@ def compute_lengths(row):
 
     return lengths
 
+
+def compute_gc_content_sequence_components(row):
+    """Compute the gc content of the cds, utr3 and utr5
+    
+    Args:
+        row (dict): row extracted from the csv file. This contains the
+        following fields: ensembl_gene_id, transcript_id, promoter, utr5, cds,
+        utr3, terminator
+
+    Returns:
+        dict: dictionary of gc content of cds, utr5 and utr3
+    """
+    utr5 = row.get("utr5")
+    cds = row.get("cds")
+    utr3 = row.get("utr3")
+
+    utr5_gc = count_gc_nucleotides(utr5)
+    cds_gc = count_gc_nucleotides(cds)
+    utr3_gc = count_gc_nucleotides(utr3)
+
+    utr5_length = len(utr5)
+    cds_length = len(cds)
+    utr3_length = len(utr3)
+
+    gc_content = {"utr5_gc":utr5_gc/utr5_length,
+                  "cds_gc":cds_gc/cds_length,
+                  "utr3_gc":utr3_gc/utr3_length}
+
+    return gc_content
+
+
+def count_gc_nucleotides(sequence):
+    """Count the number of times G and C nucleotides appear in a DNA sequence
+    
+    Args:
+        sequence (str): DNA sequence
+
+    Returns:
+        int: count of G nucleotides in the sequence
+        int: count of C nucleotides in the sequence
+    """
+    g_content = sequence.count("G")
+    c_content = sequence.count("C")
+    return g_content + c_content
 
 
 # Usage example
