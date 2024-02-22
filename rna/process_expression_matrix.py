3e249c42756074678b0874642b4e27ff93596586
Rada Djoneva <ryd23@ic.ac.uk>
2024-02-22 09:37:33 +0000

    SCRUM-38: Copied Avanijas calculate_rsd function to rna/process_expression_matrix.

--- rna/process_expression_matrix.py
+++ rna/process_expression_matrix.py
@@ -1,15 +1,25 @@
 import pandas as pd
 
 
-def calculate_rsd():
-    pass
+def calculate_rsd(df, gene_id_column):
+    # Calculate the mean and standard deviation across the row
+    df['mean'] = df.iloc[:, 1:].mean(axis=1)
+    df['std_dev'] = df.iloc[:, 1:-1].std(axis=1)
+    # Calculate the RSD (Relative Standard Deviation)
+    df['rsd'] = df['std_dev'] / df['mean']
+    return df
+
 
 def calculate_median_expression():
     pass
 
-def process_expression_matrix(file_path):
-    pass
 
+def process_expression_matrix(file_path):
+    # Apply the function to the dataframe
+    df_rsd = calculate_rsd(df, 'ensembl_gene_id')
+
+    # Filter the genes with RSD < 2
+    filtered_genes = df_rsd[df_rsd['rsd'] < 2]
 
 
 if __name__ == "__main__":
