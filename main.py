eaa577c6d6db5b9f975b6e0565991ed6982cb341
Rada Djoneva <ryd23@ic.ac.uk>
2024-03-07 09:13:56 +0000

    SCRUM-50: Removed all raw quant files from Git (left two as an examples). There will be stored in OneDrive. Updated main.py to call process_rna_expression_data and merge_datasets functions from dataset_integration.py. process_rna_expression_data function processes all raw quant files in order to obtain the median expression for all transcripts which is then merged with the DNA data.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from dna_extraction import extract_dna_data
 from rna_extraction import extract_rna_data
-from dataset_integration import merge_datasets
+from dataset_integration import merge_datasets, process_rna_expression_data
 
 
 def run_pipeline():
@@ -10,7 +10,15 @@ def run_pipeline():
     extract_dna_data()
     # extract_rna_data()
 
-    # merge_datasets()
+    # Process raw quant.sf files from the nf-core rna-seq pipeline
+    # to obtain processed median expression of transcripts
+    process_rna_expression_data()
+
+    # Merge DNA and RNA datasets for each species
+    species_names = ['chlamydomonas_reinhardtii', 'cyanidioschyzon_merolae', 'galdieria_sulphuraria',
+                     'homo_sapiens', 'saccharomyces_cerevisiae']
+    for species_name in species_names:
+        merge_datasets(species_name=species_name)
 
 
 if __name__ == "__main__":
