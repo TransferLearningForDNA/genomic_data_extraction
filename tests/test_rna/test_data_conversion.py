c89d03d9c7b4ae1aafb95248054f49ff0515108d
Avanija Menon <am1623@ic.ac.uk>
2024-04-19 20:30:55 +0100

    adding test of download functionality from mRNA_fastq_download

--- tests/test_rna/test_data_conversion.py
+++ tests/test_rna/test_data_conversion.py
@@ -2,6 +2,7 @@ import pytest
 from unittest.mock import patch, MagicMock
 import pandas as pd
 from rna.rna_download_logic.query_and_csv_production import query_and_get_srx_accession_ids, SRX_to_SRR_csv
+from rna.rna_download_logic.mRNA_fastq_download import download_sra_data
 
 @pytest.fixture
 def mock_sra_search():
@@ -17,3 +18,10 @@ def test_SRX_to_SRR_csv_writes_correct_data_to_file():
         species_srx_map = {'Homo sapiens': ['SRX123456']}
         SRX_to_SRR_csv(species_srx_map, "dummy_path.csv")
         mock_to_csv.assert_called_once()
+
+def test_download_sra_data_limit_reached():
+    with patch('rna.rna_download_logic.mRNA_fastq_download.os.path.exists') as mock_exists, \
+         patch('rna.rna_download_logic.mRNA_fastq_download.subprocess.run') as mock_run:
+        mock_exists.return_value = True
+        download_sra_data("dummy.csv", "dummy_dir", 0)
+        mock_run.assert_not_called()
