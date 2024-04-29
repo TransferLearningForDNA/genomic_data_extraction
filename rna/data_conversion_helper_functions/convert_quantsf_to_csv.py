c12deaca6792557a07ef3ed48b91eed0911d40a2
Rada Djoneva <ryd23@ic.ac.uk>
2024-04-29 10:32:06 +0100

    Removed a small print message convert sf to csv.

--- rna/data_conversion_helper_functions/convert_quantsf_to_csv.py
+++ rna/data_conversion_helper_functions/convert_quantsf_to_csv.py
@@ -60,10 +60,8 @@ def convert_quant_output_to_csv(input_path: str, output_path: str) -> None:
                 for row in tsv_reader:
                     csv_writer.writerow(row)
 
-            print(f"Data saved to {output_file_path}")
 
-
-if __name__ == "__main__":  # pragma: no cover, convert quant.sf files to csv
+if __name__ == "__main__":
     # Path to input raw quant.sf files folder
     raw_quant_path = "quant_files/raw"
     convert_all_species_files(raw_quant_path)
